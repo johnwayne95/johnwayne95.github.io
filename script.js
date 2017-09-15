eedfe7ee55d3d80f4393ddf30698b775e0acbe4e
TreyWayne95 <johnbwayne95@gmail.com>
2017-09-15 16:06:01 -0500

    FUCK THIS UGH

--- script.js
+++ script.js
@@ -1,21 +1,67 @@
-var scene = new THREE.Scene(),
-            camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
-            renderer = new THREE.WebGLRenderer();  
+var scene;
 
-      renderer.setSize(window.innerWidth, window.innerHeight); 
-      document.body.appendChild(renderer.domElement); 
-      var cube = new THREE.Mesh(
-                new THREE.CubeGeometry(2,2,2),
-                new THREE.MeshBasicMaterial({wireframe: true, color: 0xFFFFFF}
-            )); 
+var camera;
 
-      scene.add(cube); 
-      camera.position.z = 5;
+initializeScene();
 
-      var render = function () { 
-        requestAnimationFrame(render); 
-        cube.rotation.x += 0.01; 
-        cube.rotation.y += 0.01; 
-        renderer.render(scene, camera); 
-      }; 
-      render();
+renderScene();
+
+function initializeScene() {
+    if(Detector.webgl) {
+        renderer = new THREE.WebGLRenderer({antialias:true});
+    } else {
+        renderer = new THREE.CanvasRenderer();
+    }
+
+    renderer.setClearColor(0x000000, 1);
+
+    canvasWidth = window.innerWidth;
+    canvasHeight = window.innerHeight;
+
+    renderer.setsize(canvasWidth, canvasHeight);
+
+    document.getElementById("WebGLCanvas").appendChild(renderer.domElement);
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(45, canvasWidth / canvasHeight, 1, 100);
+    camera.position.set(0, 0, 10);
+    camera.lookAt(scene.position); 
+    scene.add(camera);
+
+    var triangleGeometry = new THREE.Geometry(); 
+    triangleGeometry.vertices.push(new THREE.Vector3( 0.0,  1.0, 0.0)); 
+    triangleGeometry.vertices.push(new THREE.Vector3(-1.0, -1.0, 0.0)); 
+    triangleGeometry.vertices.push(new THREE.Vector3( 1.0, -1.0, 0.0)); 
+    triangleGeometry.faces.push(new THREE.Face3(0, 1, 2));
+
+    var triangleMaterial = new THREE.MeshBasicMaterial({ 
+        color:0xFFFFFF, 
+        side:THREE.DoubleSide 
+    });
+
+    var triangleMesh = new THREE.Mesh(triangleGeometry, triangleMaterial); 
+    triangleMesh.position.set(-1.5, 0.0, 4.0);
+    scene.add(triangleMesh);
+
+    var squareGeometry = new THREE.Geometry(); 
+    squareGeometry.vertices.push(new THREE.Vector3(-1.0,  1.0, 0.0)); 
+    squareGeometry.vertices.push(new THREE.Vector3( 1.0,  1.0, 0.0)); 
+    squareGeometry.vertices.push(new THREE.Vector3( 1.0, -1.0, 0.0)); 
+    squareGeometry.vertices.push(new THREE.Vector3(-1.0, -1.0, 0.0)); 
+    squareGeometry.faces.push(new THREE.Face3(0, 1, 2)); 
+    squareGeometry.faces.push(new THREE.Face3(0, 2, 3)); 
+
+    var squareMaterial = new THREE.MeshBasicMaterial({ 
+        color:0xFFFFFF, 
+        side:THREE.DoubleSide 
+    });
+
+    var squareMesh = new THREE.Mesh(squareGeometry, squareMaterial); 
+        squareMesh.position.set(1.5, 0.0, 4.0); 
+        scene.add(squareMesh); 
+}
+
+function renderScene() {
+    renderer.render(scene, camera);
+}
