92eb101f4eb422fe00720e3cb5d40176a22e058b
TreyWayne95 <johnbwayne95@gmail.com>
2017-09-15 14:51:31 -0500

    dfgdf

--- script.js
+++ script.js
@@ -1,90 +1,21 @@
-    // Set the scene size.
-    const WIDTH = window.innerWidth;
-    const HEIGHT = window.innerHeight;
+var scene = new THREE.Scene(),
+            camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
+            renderer = new THREE.WebGLRenderer();  
 
-    // Set some camera attributes.
-    const VIEW_ANGLE = 45;
-    const ASPECT = WIDTH / HEIGHT;
-    const NEAR = 0.1;
-    const FAR = 10000;
+      renderer.setSize(window.innerWidth, window.innerHeight); 
+      document.body.appendChild(renderer.domElement); 
+      var cube = new THREE.Mesh(
+                new THREE.CubeGeometry(2,2,2),
+                new THREE.MeshBasicMaterial({wireframe: true, color: 0xFFFFFF}
+            )); 
 
-    // Get the DOM element to attach to
-    const container =
-        document.querySelector('#container');
+      scene.add(cube); 
+      camera.position.z = 5;
 
-    // Create a WebGL renderer, camera
-    // and a scene
-    const renderer = new THREE.WebGLRenderer();
-    const camera =
-        new THREE.PerspectiveCamera(
-            VIEW_ANGLE,
-            ASPECT,
-            NEAR,
-            FAR
-        );
-
-    const scene = new THREE.Scene();
-
-    // Add the camera to the scene.
-    scene.add(camera);
-
-    // Start the renderer.
-    renderer.setSize(WIDTH, HEIGHT);
-
-    // Attach the renderer-supplied
-    // DOM element.
-    container.appendChild(renderer.domElement);
-
-    // create a point light
-    const pointLight =
-      new THREE.PointLight(0xFFFFFF);
-
-    // set its position
-    pointLight.position.x = 10;
-    pointLight.position.y = 50;
-    pointLight.position.z = 130;
-
-    // add to the scene
-    scene.add(pointLight);
-
-    // create the sphere's material
-    const sphereMaterial =
-      new THREE.MeshLambertMaterial(
-        {
-          color: 0xCC0000
-        });
-
-    // Set up the sphere vars
-    const RADIUS = 50;
-    const SEGMENTS = 16;
-    const RINGS = 16;
-
-    // Create a new mesh with
-    // sphere geometry - we will cover
-    // the sphereMaterial next!
-    const sphere = new THREE.Mesh(
-
-      new THREE.SphereGeometry(
-        RADIUS,
-        SEGMENTS,
-        RINGS),
-
-      sphereMaterial);
-
-    // Move the Sphere back in Z so we
-    // can see it.
-    sphere.position.z = -300;
-
-    // Finally, add the sphere to the scene.
-    scene.add(sphere);
-
-    function update () {
-      // Draw!
-      renderer.render(scene, camera);
-
-      // Schedule the next frame.
-      requestAnimationFrame(update);
-    }
-
-    // Schedule the first frame.
-    requestAnimationFrame(update);
+      var render = function () { 
+        requestAnimationFrame(render); 
+        cube.rotation.x += 0.01; 
+        cube.rotation.y += 0.01; 
+        renderer.render(scene, camera); 
+      }; 
+      render();
