81ae23701fce93d45013102c5876f8bcb3308175
TreyWayne95 <johnbwayne95@gmail.com>
2017-09-15 17:05:39 -0500

    FUCK YEAH

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-			var scene = new THREE.Scene();
-			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var scene = new THREE.Scene(),
+            camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
+            renderer = new THREE.WebGLRenderer( { alpha: true });  
 
-			var renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
+      renderer.setSize(window.innerWidth, window.innerHeight); 
+      document.body.appendChild(renderer.domElement); 
+      var ico = new THREE.Mesh(
+                new THREE.IcosahedronGeometry(3,1),
+                new THREE.MeshBasicMaterial({wireframe: true, color: 0xff8080}
+            )); 
 
-			var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			var cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
+      scene.add(ico); 
+      camera.position.z = 5;
 
-			camera.position.z = 5;
-
-			var animate = function () {
-				requestAnimationFrame( animate );
-
-				cube.rotation.x += 0.1;
-				cube.rotation.y += 0.1;
-
-				renderer.render(scene, camera);
-			};
-
-			animate();
+      var render = function () { 
+        requestAnimationFrame(render); 
+        ico.rotation.x += 0.002; 
+        ico.rotation.y += 0.003; 
+        renderer.render(scene, camera); 
+      }; 
+      render();
