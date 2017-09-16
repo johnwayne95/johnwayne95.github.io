21b98e8b7863ab190c25557601f8d9b9049b123f
TreyWayne95 <johnbwayne95@gmail.com>
2017-09-15 20:43:28 -0500

    go through pls

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 var scene = new THREE.Scene(),
             camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 10)
-            renderer = new THREE.WebGLRenderer( { alpha: true });  
+            renderer = new THREE.WebGLRenderer( { alpha: true }, {antialias: true});  
 
       renderer.setSize(window.innerWidth, window.innerHeight); 
       document.body.appendChild(renderer.domElement); 
